0070ecbec94bbda4cf54483ba4da26215c669ee3
olman-dep <olmanzapata22@gmail.com>
2024-09-14 09:05:06 -0500

    cambios en pol

--- Polimorfismo.cpp
+++ Polimorfismo.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
 
+using namespace std;//hola cambios
+
+class vehiculo
+{
+public:
+    virtual void acelerar() = 0;
+    virtual void frenar() = 0;
+};
+
+class coche : public vehiculo
+{
+public:
+    void acelerar() override
+    {
+        cout << " el coche esta acelerando " << endl;
+    }
+    void frenar() override
+    {
+        cout << " el coche freno " << endl;
+    }
+};
+
+class moto : public vehiculo
+{
+public:
+    void acelerar() override
+    {
+        cout << " La moto esta acelerando "<<endl;
+    }
+    void frenar() override
+    {
+        cout << " la moto esta frenando "<< endl;
+    }
+};
+
+int main()
+{
+
+
+
+}
