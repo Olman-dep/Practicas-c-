8e0cd44838c008f96f1089e40b1c9dbd2425ffe5
olman-dep <olmanzapata22@gmail.com>
2024-09-15 14:26:44 -0500

    Se mejora el codigo y se entiende como se usa el polimorfimos atravez de objetos y punteros

--- Polimorfismo.cpp
+++ Polimorfismo.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-using namespace std;//hola cambios
+using namespace std; // hola cambios
 
 class vehiculo
 {
@@ -27,17 +27,31 @@ class moto : public vehiculo
 public:
     void acelerar() override
     {
-        cout << " La moto esta acelerando "<<endl;
+        cout << " La moto esta acelerando " << endl;
     }
     void frenar() override
     {
-        cout << " la moto esta frenando "<< endl;
+        cout << " la moto esta frenando " << endl;
     }
 };
 
+class bicicleta : public vehiculo
+{
+public:
+    void acelerar() override
+    {
+        cout << "se esta pedaleando mas rapido" << endl;
+    }
+    void frenar() override
+    {
+        cout << " la bicicleta esta frenando " << endl;
+    }
+};
 int main()
 {
-
-
-
-}
+    vehiculo* mivehiculo = new coche();
+    vehiculo* mivehiculo2 = new moto();
+    mivehiculo-> frenar();
+    
+    return 0;
+    }
